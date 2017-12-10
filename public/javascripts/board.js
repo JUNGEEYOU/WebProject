d8b2a49a792ab04c8ecfe5cee894b54d2feb3ab6
JUNGEEYOU <junge2u@naver.com>
2017-12-11 00:39:13 +0900

    comment 작업중

--- public/javascripts/board.js
+++ public/javascripts/board.js
@@ -6,3 +6,16 @@ function cancelAdd(){
 
 window.location.href = '/gallery';
 }
+
+
+
+function myFunction(name, info, img, gallery_id) {
+    
+        var commnetdata ="<tr><th>User name</th><th>comment</th></tr>";
+    
+        document.getElementById("myModalLabel").innerHTML =  name;
+        document.getElementById("info").innerHTML =  info;
+        document.getElementById("gallery_img").src =img;
+    
+        console.log("idid",gallery_id);
+    }
