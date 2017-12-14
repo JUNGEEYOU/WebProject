6ed940ec70e3dfdf9fecafb5f49f835d23da70f4
JUNGEEYOU <junge2u@naver.com>
2017-12-14 17:39:17 +0900

    이메일 보내기

--- routes/contact.js
+++ routes/contact.js
@@ -1,10 +1,50 @@
 
 var express = require('express');
+var nodemailer = require('nodemailer');
+
 var router = express.Router();
+
+
+
+var transporter = nodemailer.createTransport({
+  service: 'naver',
+  auth: {
+      user: 'junge2u@naver.com',
+      pass: 'jungee135~'
+  }
+});
+
+
 
 router.get('/', function(req, res, next) {
   res.render('contact',{user:req.user});
 });
+
+router.post('/', function(req, res, next){
+  var input = JSON.parse(JSON.stringify(req.body));
+
+  var mailOptions = {
+    from: input.email,
+    to: 'junge2u@naver.com',
+    subject: input.subject,
+    text: 'name: '+ input.name + '\nphone: '+ input.tel + '\nmessage: ' + input.message
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent! : ' + info.response);
+    }
+    transporter.close();
+  });
+  res.send("<script>alert('send email!'); location.href='/';</script>");
+  res.redirect('/');
+
+});
+
+
+
 router.get('/logout', function(req, res, next) {
   req.session.destroy(function(err){
     if(err){console.log(err)}
@@ -14,4 +54,9 @@ router.get('/logout', function(req, res, next) {
   );
   res.render('/', {user:req.user});
 });
+
+
+
+
+
 module.exports = router;
